b6d638cdae29b890d91bbcf9952dbd9063fb3a9e
Brady Reid <bradyreid2004@gmail.com>
2025-12-28 21:48:37 -0700

    Add AWS CloudTrail resource configuration

--- terraform/cloudtrail.tf
+++ terraform/cloudtrail.tf
@@ -1,1 +1,7 @@
-
+resource "aws_cloudtrail" "this" {
+  name                          = "${var.project_name}-trail"
+  s3_bucket_name                = aws_s3_bucket.logs.bucket
+  include_global_service_events = true
+  is_multi_region_trail         = true
+  enable_logging                = true
+}
