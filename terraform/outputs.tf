0010712dcb8c9741c1fbc4615b8845cf252f2001
Brady Reid <bradyreid2004@gmail.com>
2025-12-28 21:48:57 -0700

    Add outputs for VPC ID, S3 bucket, and EC2 role

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
+output "vpc_id" {
+  description = "VPC ID"
+  value       = aws_vpc.this.id
+}
 
+output "log_bucket_name" {
+  description = "S3 bucket used for CloudTrail logs"
+  value       = aws_s3_bucket.logs.bucket
+}
+
+output "ec2_role_name" {
+  description = "IAM role assigned to EC2"
+  value       = aws_iam_role.ec2_role.name
+}
