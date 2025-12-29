0923f473cb6c4bcb283c5f86bf65a6502be55636
Brady Reid <bradyreid2004@gmail.com>
2025-12-28 21:47:59 -0700

    Add S3 bucket and policy for CloudTrail logs

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,1 +1,46 @@
+resource "aws_s3_bucket" "logs" {
+  bucket        = "${var.project_name}-logs"
+  force_destroy = true
+}
 
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket                  = aws_s3_bucket.logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "cloudtrail_bucket_policy" {
+  statement {
+    sid     = "AllowCloudTrailWrite"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.logs.arn}/*"]
+  }
+
+  statement {
+    sid     = "ExplicitDenyAllOthers"
+    effect  = "Deny"
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.logs.arn,
+      "${aws_s3_bucket.logs.arn}/*"
+    ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "logs" {
+  bucket = aws_s3_bucket.logs.id
+  policy = data.aws_iam_policy_document.cloudtrail_bucket_policy.json
+}
