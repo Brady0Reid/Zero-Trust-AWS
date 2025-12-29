6d25841aec4ddd7f8b9d588a5ec5f34253398bdb
Brady Reid <bradyreid2004@gmail.com>
2025-12-28 21:47:12 -0700

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,1 +1,28 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+
+  tags = {
+    Name = "${var.project_name}-private-subnet"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.this.id
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+
+  tags = {
+    Name = "${var.project_name}-s3-endpoint"
+  }
+}
