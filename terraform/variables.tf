1537b6d6e5c10a91ff9c6b1c3955415a4ad695a6
Brady Reid <bradyreid2004@gmail.com>
2025-12-28 21:46:42 -0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "project_name" {
+  description = "Project name prefix"
+  type        = string
+  default     = "zero-trust-aws"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
